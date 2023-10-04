949e40b4226ab09db9c1c4546c95047340bea235
Jesus Salomon <salomonj1121@gmail.com>
2023-10-03 19:59:05 -0500

    day1 part 2 done

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ function parseInput(input) {
 }
 
 function findElfWithMostcalories(elfGroups) {
-    return Math.max(...elfGroups.map((elfGroup) => elfGroup.reduce((a, b) => a + b, 0)));
+    return Math.max(...elfGroups.map((elfGroup) => getTotalCalories(elfGroup)));
+}
+
+function findTop3ElfWithMostcalories(elfGroups) {
+    const totalCaloriesPerElf = elfGroups.map(elfGroup => getTotalCalories(elfGroup));
+    const sortedCaloriesPerElf = totalCaloriesPerElf.sort((a, b) => b - a);
+    return getTotalCalories(sortedCaloriesPerElf.slice(0, 3));
+}
+
+function getTotalCalories(elfGroup) {
+    return elfGroup.reduce((a, b) => a + b, 0);
 }
 
 function main() {
     try {
         const parsedInput = parseInput(data);
         const maxCalories = findElfWithMostcalories(parsedInput);
+        const top3CaloriesTotal = findTop3ElfWithMostcalories(parsedInput);
+
         console.log('The Elf with the most calories has', maxCalories, 'calories');
+        console.log('The total of the top 3 Elves with the most calories is', top3CaloriesTotal, 'calories');
+
     } catch (e) {
         console.error(e);
     }   
 }
 
 main();
+
